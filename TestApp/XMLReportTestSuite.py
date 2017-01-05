65b8679045280147148c1ed5207b5e5e0debe6c1
tkumark <t1279k@gmail.com>
2017-01-05 09:43:50 +0530

    added shippable directory

--- TestApp/XMLReportTestSuite.py
+++ TestApp/XMLReportTestSuite.py
@@ -8,4 +8,5 @@ remote_tests = unittest.TestLoader().loadTestsFromTestCase(TestSite)
 # create a test suite combining search_test and home_page_test
 smoke_tests = unittest.TestSuite([remote_tests])
 # run the suite
-xmlrunner.XMLTestRunner(verbosity=2, output='test-reports').run(smoke_tests)
+#http://docs.shippable.com/ci/advancedOptions/testCoverage/
+xmlrunner.XMLTestRunner(verbosity=2, output='shippable').run(smoke_tests)
